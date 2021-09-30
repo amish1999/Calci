b0c5f1192d67de05825aa649ca45299c93966db5
Sayan Pal <48083671+sayanp99@users.noreply.github.com>
2021-10-01 02:01:30 +0530

    Update ExampleInstrumentedTest.java
    app context added in manifest

--- app/src/androidTest/java/com/example/calci/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/calci/ExampleInstrumentedTest.java
@@ -8,6 +8,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-<!--------->
-    
-    
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.calci", appContext.getPackageName());
+    }
+}
